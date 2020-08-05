a3750140b140d147cb7bbabbac2294c5492b4edc
JIM <47224583+JimByte@users.noreply.github.com>
2020-08-05 15:39:32 +0430

    Create encod.py

---
+++ encod.py
@@ -0,0 +1,29 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+RotorKey = 'dwebakxhzrnplyuqmjcstgivofgwutdzqlyckxapimvbnsorjehfhqcbuonrktefzvwidpxljsamyg'
+def Reflector(R):
+    return alphabet[(26-alphabet.find(R))-1]
+def OneCharUp(R):
+    R2 = []
+    for i in range(25):
+        i = i+1
+        R2.append(R[i])
+    R2.append(R[0])
+    return R2
+text='hi'
+Cipher=''
+R1 = list(RotorKey[0:26])
+R2 = list(RotorKey[26:52])
+R3 = list(RotorKey[52:78])
+CR1 = 0
+CR2 = 0
+CR3 = 0
+for i in text:
+    E1 = R1[list(alphabet).index(i)]
+    E2 = R2[list(alphabet).index(E1)]
+    E3 = R3[list(alphabet).index(E2)]
+    E4 = Reflector(E3)
+    Cipher+=E4
+    CR1 += 1
+    R1 = OneCharUp(R1)
+    R1
+print(Cipher)
